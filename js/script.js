c5df5f4a1f77fe111879cef1662c2e7476f3319a
Deepak <deepak3823@gmail.com>
2017-05-30 21:42:06 +0530

    Added slide menu on the left.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var mainBody = document.getElementsByClassName('container')[0];
+mainBody.addEventListener('click', function(){
+	slideMenu.classList.remove('open');
+});
+var menuButton = document.getElementById('menu');
+menuButton.addEventListener('click', function(e){
+	slideMenu.classList.toggle('open');
+});
+
