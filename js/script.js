96718e2e48479db821ec25b32057956c3decf717
Deepak <deepak3823@gmail.com>
2017-05-30 22:32:33 +0530

    Added functions to links in the slidemenu.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,21 @@ var mainBody = document.getElementsByClassName('container')[0];
 mainBody.addEventListener('click', function(){
 	slideMenu.classList.remove('open');
 });
+
+
 var menuButton = document.getElementById('menu');
 menuButton.addEventListener('click', function(e){
 	slideMenu.classList.toggle('open');
 	e.stopPropagation();
 });
 
+
+var mobileView = document.getElementById('mobileView');
+mobileView.addEventListener('click', function(){
+	mainBody.style.width = '350px';
+});
+
+var desktopView = document.getElementById('desktopView');
+desktopView.addEventListener('click', function(){
+	mainBody.style.width = '940px';
+});
